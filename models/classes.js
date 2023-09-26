fd0c948c5ef9b7d3d8ab8c734d17565cc2299376
Gabriel <gabrielenriquecas@gmail.com>
2023-09-26 15:18:16 -0400

    models added

---
+++ models/classes.js
@@ -0,0 +1,10 @@
+const schemaClass = new {
+    weekDay: String,
+    schedule: String('ex. from 9am to 12pm'),
+    dayStart: Date,
+    course: String,
+    modality: String('presencial, semi, online'),
+    availability: Boolean,
+};
+
+schemaClass;
---
+++ models/users.js
@@ -0,0 +1,9 @@
+const user = new {
+    name: String,
+    surname: String,
+    phoneNumber: String,
+    courseID: String,
+    module: Number,
+};
+
+user;
