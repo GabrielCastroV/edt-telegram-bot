4c1c3431b9ce72da63901cc411ad14ff90a7bc56
Gabriel <gabrielenriquecas@gmail.com>
2023-11-03 16:49:10 -0400

    edt online improved

--- actions/edtonline.js
+++ actions/edtonline.js
@@ -196,7 +196,7 @@ Nuestro objetivo es desarrollar y ayudar a profesionales que puedan trabajar en 
     
 (⏳ Duración: 6 Meses)
 `;
-    menuOnline(ctx, info, 'plan_de_estudios_pr_on', 'como_funciona_pr_on', 'costo_matricula_pr_on', 'horarios_pr_on', 'volver_edt_online', 'metodo_pago_pr_on');
+    menuOnline(ctx, info, 'plan_de_estudios_pr_on', 'como_funciona_pr_on', 'costo_matricula_pr_on', 'horarios_pr_on', 'online', 'metodo_pago_pr_on');
 });
 bot.action('plan_de_estudios_pr_on', async (ctx) => {
     const info = `
@@ -213,7 +213,7 @@ bot.action('plan_de_estudios_pr_on', async (ctx) => {
 
     Durante todo el BootCamp desarrollarás al menos 10 proyectos reales.  
     `;
-    planEstudiosOnline(ctx, info, 'volver_edt_online_pr');
+    planEstudiosOnline(ctx, info, 'programacion_on');
 });
 bot.action('como_funciona_pr_on', async (ctx) => {
     const info = `
@@ -223,7 +223,7 @@ bot.action('como_funciona_pr_on', async (ctx) => {
 
     Estarás realizando alrededor de 10 proyectos y al final deberás desarrollar tu propia app la cual deberás presentar en un Demo Day 
     `;
-    comoFuncionaOnline(ctx, info, 'volver_edt_online_pr');
+    comoFuncionaOnline(ctx, info, 'programacion_on');
 });
 bot.action('costo_matricula_pr_on', async (ctx) => {
     const info = `
@@ -233,35 +233,19 @@ bot.action('costo_matricula_pr_on', async (ctx) => {
 
     NOTA: Podrás acceder siempre al campus pero debes estar solventes para desbloquear los módulos. 
     `;
-    costoMatriculaOnline(ctx, info, 'volver_edt_online_pr');
+    costoMatriculaOnline(ctx, info, 'programacion_on');
 });
 bot.action('horarios_pr_on', async (ctx) => {
-    schedulesOnline(ctx, 'volver_edt_online_pr');
+    schedulesOnline(ctx, 'programacion_on');
 });
 bot.action('metodo_pago_pr_on', async (ctx) => {
-    paymentMethod(ctx, 'inscribir_pr_on', 'pago_movil_pr_on', 'volver_edt_online_pr');
+    paymentMethod(ctx, 'inscribir_pr_on', 'pago_movil_pr_on', 'programacion_on');
 });
 bot.action('pago_movil_pr_on', async (ctx) => {
     pagoMovil(ctx, 'Programación Full Stack Online 🚀', 100);
 });
 bot.action('inscribir_pr_on', async (ctx) => {
     signUp(ctx, 'Programación Full Stack Online 🚀', 10000, 'https://i.imgur.com/hvnITG8.jpg');
-});
-bot.action('volver_edt_online_pr', async (ctx) => {
-    const info = `
-    Programación Full Stack - Online
-    
-    Una de las habilidades de mayor demanda laboral es ¡Programador!
-    
-    Lo sabemos. Ya nosotros estamos en la industria. Y hemos desarrollado un programa con mas de 10 proyectos prácticos, clones, aplicaciones, webs, bases de datos, entre otros que son los pilares para aprender Programación Full Stack.
-    
-    Al finalizar el curso tendrás tu perfil en LinkedIn activo, te guiamos en la creación de tu portafolio profesional en Git Hub y asesoramos para ofertar en las principales plataformas FreeLancer.
-    
-    Nuestro objetivo es desarrollar y ayudar a profesionales que puedan trabajar en pijama, acceder a un mercado laboral desde cualquier parte del mundo; que desarrollen proyectos o emprendimiento basado en tecnología.
-    
-    (⏳ Duración: 6 Meses)
-    `;
-    menuOnline(ctx, info, 'plan_de_estudios_pr_on', 'como_funciona_pr_on', 'costo_matricula_pr_on', 'horarios_pr_on', 'volver_edt_online', 'metodo_pago_pr_on');
 });
 
 // Marketing y Redes Sociales
@@ -282,7 +266,7 @@ Una carrera con aval universitario y programa de pasantía, te dejo esto como no
 
 (⏳ Duración: 6 Meses)
     `;
-    menuOnline(ctx, info, 'plan_de_estudios_mr_on', 'como_funciona_mr_on', 'costo_matricula_mr_on', 'horarios_mr_on', 'volver_edt_online', 'metodo_pago_mr_on');
+    menuOnline(ctx, info, 'plan_de_estudios_mr_on', 'como_funciona_mr_on', 'costo_matricula_mr_on', 'horarios_mr_on', 'online', 'metodo_pago_mr_on');
 });
 bot.action('plan_de_estudios_mr_on', async (ctx) => {
     const info = `
@@ -306,7 +290,7 @@ bot.action('plan_de_estudios_mr_on', async (ctx) => {
     ✔ Equipo social media dentro de la empresa
     ✔ Plan de marketing ¿Cómo lograr generar prospectos?
     `;
-    planEstudiosOnline(ctx, info, 'volver_edt_online_mr');
+    planEstudiosOnline(ctx, info, 'marketing_redes_on');
 });
 bot.action('como_funciona_mr_on', async (ctx) => {
     const info = `
@@ -314,7 +298,7 @@ bot.action('como_funciona_mr_on', async (ctx) => {
 
     100 % ONLINE - Acceso al campus virtual con clases en video y sesiones en vivo con un tutor.
     `;
-    comoFuncionaOnline(ctx, info, 'volver_edt_online_mr');
+    comoFuncionaOnline(ctx, info, 'marketing_redes_on');
 });
 bot.action('costo_matricula_mr_on', async (ctx) => {
     const info = `
@@ -324,13 +308,13 @@ bot.action('costo_matricula_mr_on', async (ctx) => {
 
     Pago 1 - 100 $ y luego 5 cuotas de 80 $ c/u
     `;
-    costoMatriculaOnline(ctx, info, 'volver_edt_online_mr');
+    costoMatriculaOnline(ctx, info, 'marketing_redes_on');
 });
 bot.action('horarios_mr_on', async (ctx) => {
-    schedulesOnline(ctx, 'volver_edt_online_mr');
+    schedulesOnline(ctx, 'marketing_redes_on');
 });
 bot.action('metodo_pago_mr_on', async (ctx) => {
-    paymentMethod(ctx, 'inscribir_mr_on', 'pago_movil_mr_on', 'volver_edt_online_mr');
+    paymentMethod(ctx, 'inscribir_mr_on', 'pago_movil_mr_on', 'marketing_redes_on');
 });
 bot.action('pago_movil_mr_on', async (ctx) => {
     pagoMovil(ctx, 'Marketing y Redes Sociales Online 📈', 100);
@@ -338,43 +322,7 @@ bot.action('pago_movil_mr_on', async (ctx) => {
 bot.action('inscribir_mr_on', async (ctx) => {
     signUp(ctx, 'Marketing y Redes Sociales Online', 10000, 'https://i.imgur.com/34YMyWK.jpg');
 });
-bot.action('volver_edt_online_mr', async (ctx) => {
-    const info = `
-    Marketing Digital y Redes Sociales - Online
 
-La verdad no te diría mucho si te digo que el Marketing Digital es uno de los sectores de mayor crecimiento; no solo laboral, sino en relación a crear tu propia agencia o trabajar como FreeLancer desde cualquier parte del mundo. 
-
-Claro, es importante estudiar, prepararse. El mercado es super competitivo. Por lo tanto hemos diseñado una carrera técnica brutal.
-
-Te explico 
-
-Hay un montón de cursos aislados. Nosotros en una sola carrera, metimos las 4 competencias claves de un marketing: Estrategia, Manejo de crisis, Herramientas de gestión y la maquina de generar prospectos.
-
-Una carrera con aval universitario y programa de pasantía, te dejo esto como nota final.
-
-(⏳ Duración: 6 Meses)
-    `;
-    menuOnline(ctx, info, 'plan_de_estudios_mr_on', 'como_funciona_mr_on', 'costo_matricula_mr_on', 'horarios_mr_on', 'volver_edt_online', 'metodo_pago_mr_on');
-});
-
-// Volver a los cursos online
-
-bot.action('volver_edt_online', async (ctx) => {
-    try {
-        await ctx.deleteMessage();
-    } catch (error) {
-        console.log(error);
-    }
-    await ctx.telegram.sendMessage(ctx.chat.id, 'Estos son los cursos online disponibles:',
-        {
-            reply_markup: {
-                inline_keyboard: [
-                    [{ text: 'Programación 💻', callback_data: 'programacion_on' }, { text: 'Marketing y Redes Sociales 📈', callback_data: 'marketing_redes_on' }],
-                    [{ text: '< Volver', callback_data: 'volver_cursos' }],
-                ],
-            },
-        });
-});
 
 bot.use(middleware);
 module.exports = bot.middleware();
