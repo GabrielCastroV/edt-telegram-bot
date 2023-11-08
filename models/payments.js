f53b081cc578480a8f43f5280d2b93f5ec5ad737
Gabriel <gabrielenriquecas@gmail.com>
2023-11-08 16:57:37 -0400

    ref added

--- models/payments.js
+++ models/payments.js
@@ -2,10 +2,11 @@ const mongoose = require('mongoose');
 
 const paymentsSchema = new mongoose.Schema({
     email: String,
+    is_registration: Boolean,
     course: String,
     amount: Number,
+    ref_number: Number,
     verified: Boolean,
-
 });
 
 const Payments = mongoose.model('Payments', paymentsSchema);
